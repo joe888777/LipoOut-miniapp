7411f5374ec4c555d85627760f3f4720b7e4e45b
joeAtl <joe.chen@atlancube.com>
2024-10-31 17:35:50 +0800

    feat: add telegram username in mini-app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 "use client"
 import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react';
 import { useRouter } from 'next/navigation';
+import Script from 'next/dist/client/script';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
   const router = useRouter();
+  const [user, setUser] = useState<any>({});
 
   const navigatePage = (page: string) => {
     router.push(`/${page}`);
   };
+
+  useEffect(() => {
+    // Check if the Telegram Web App API is available
+    if (window.Telegram && window.Telegram?.WebApp) {
+      const tg = window.Telegram.WebApp;
+
+      // Get user information
+      const user = tg.initDataUnsafe.user;
+
+      // Check if the user has a username
+      if (user?.username) {
+        setUser(user);
+      } else {
+        setUser({username: "no user name"});
+      }
+
+      // Optionally, you can also get other user information
+      // console.log('User ID:', user?.id);
+      // console.log('First Name:', user?.first_name);
+      // console.log('Last Name:', user?.last_name);
+    }
+  }, []);
 
   return (
     <TonConnectUIProvider
@@ -29,8 +54,20 @@ const Home = () => {
         twaReturnUrl: 'https://t.me/https://t.me/beshinebot/myapp'
     }}
     >
+      <head>
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+      </head>
       <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black">
-        <TonConnectButton className='my-2' />
+        {/* <TonConnectButton className='my-2' /> */}
+        <div className='text-white'>
+          {/* {`${user.username}`}
+          {`${user.id}`}
+          {`${user.first_name}`}
+          {`${user.last_name}`} */}
+        </div>
       </div>
     </TonConnectUIProvider>
   );
